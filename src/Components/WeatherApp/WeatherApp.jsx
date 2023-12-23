ffdb7edf5c9be43e25d0afe00d1af39b012043d5
meliance <melakmu66@gmail.com>
2023-12-23 08:37:28 +0300

    Components folder created

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './WeatherApp.css'
+
+const WetherApp = () => {
+  return (
+    <div className='container'>
+      <div className="top-bar">
+        
+      </div>
+      <h1>Welcome to ...</h1>
+    </div>
+  )
+}
+
+export default WetherApp
