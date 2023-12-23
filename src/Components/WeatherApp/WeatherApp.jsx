7965247dcf94a73aebddca17fbc09029dad1d156
meliance <melakmu66@gmail.com>
2023-12-23 10:13:27 +0300

    Wethe app file created

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import './WeatherApp.css'
 
+import search_icon from '../Assets/search.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import cloud_icon from '../Assets/cloud.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+import humidity_icon from '../Assets/humidity.png'
+import clear_icon from '../Assets/clear.png'
+
 const WetherApp = () => {
   return (
     <div className='container'>
+       <h1>Welcome to ...</h1>
       <div className="top-bar">
-        
+        <input type="text" className="cityInput" placeholder='Search City' />
+        <div className="search-icon">
+          <img src={search_icon} alt="" />
+        </div>
       </div>
-      <h1>Welcome to ...</h1>
+      
+      <div className="weather-image">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24°c</div>
+      <div className="weather-location">Addis Ababa</div>
+     
     </div>
   )
 }
